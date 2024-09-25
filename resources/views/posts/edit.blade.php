dc3b162836b8d9e394628817355b6ee0630fe0ca
Muflih Raihan <muflihraihan693@gmail.com>
2024-09-25 19:47:53 +0700

    uhuyyyyy

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,7 +9,7 @@
                     <div class="card-body">
                         <form action="{{ route('posts.update', $post->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
-                            @method('GET')
+                            @method('PUT')
 
                             <div class="form-group">
                                 <label class="font-weight-bold">GAMBAR</label>
